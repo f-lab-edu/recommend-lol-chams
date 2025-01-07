79138e56fbf49ba97b19851a52eb35753986b7cc
khyeji98 <gpwl98wkd@gmail.com>
2025-01-07 22:55:30 +0900

    First Commit

---
+++ RecommendLoLChams-Flab/RecommendLoLChams_FlabApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecommendLoLChams_FlabApp.swift
+//  RecommendLoLChams-Flab
+//
+//  Created by 김혜지 on 1/7/25.
+//
+
+import SwiftUI
+
+@main
+struct RecommendLoLChams_FlabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
