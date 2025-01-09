c91952157d81214460e28e424bc899037583ff99
khyeji98 <gpwl981207@naver.com>
2025-01-09 18:28:52 +0900

    [R-001] Firebase Crashlytics 의존성 추가

--- RecommendLoLChams-Flab/RecommendLoLChamsApp.swift
+++ RecommendLoLChams-Flab/RecommendLoLChamsApp.swift
@@ -5,10 +5,24 @@
 //  Created by 김혜지 on 1/7/25.
 //
 
+import FirebaseCore
+
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct RecommendLoLChamsApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
