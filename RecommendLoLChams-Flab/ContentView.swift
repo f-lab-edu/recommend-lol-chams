a6e5039f893618ab4892044f87f0be95fd499b94
khyeji98 <gpwl981207@naver.com>
2025-01-17 21:09:06 +0900

    [R-002] 소환사 검색 UI 구현

--- RecommendLoLChams-Flab/ContentView.swift
+++ RecommendLoLChams-Flab/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var keyword: String = ""
+    @State private var results: [String] = []
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            searchBar
+            searchView
         }
         .padding()
+    }
+
+    private var searchBar: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+
+            TextField("소환사 ID를 입력해 주세요", text: $keyword)
+
+            if !keyword.isEmpty {
+                Button {
+                    keyword = ""
+                } label: {
+                    Image(systemName: "xmark.circle.fill")
+                }
+            }
+        }
+    }
+
+    private var searchView: some View {
+        List(results, id: \.self) { item in
+            itemRow(item)
+        }
+    }
+
+    private func itemRow(_ content: String) -> some View {
+        Text(content)
     }
 }
 
