9628a6aaa30c0ea68d870c95884527873ea8ede1
khyeji98 <gpwl981207@naver.com>
2025-01-18 13:40:37 +0900

    [R-003] 소환사 id 검색 api 연동
    - dto, entity 미완

---
+++ RecommendLoLChams-Flab/SearchViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  SearchViewModel.swift
+//  RecommendLoLChams-Flab
+//
+//  Created by 김혜지 on 1/18/25.
+//
+
+import Foundation
+
+final class SearchViewModel: ObservableObject {
+    func search async {
+        
+    }
+}
