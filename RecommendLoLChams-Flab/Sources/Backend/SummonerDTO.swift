d6eb32a223aa1196fce15caa8513e2121ec93f9a
khyeji98 <gpwl981207@naver.com>
2025-02-21 17:59:50 +0900

    [R-003] 소환사 검색 로직 구현

---
+++ RecommendLoLChams-Flab/Sources/Backend/SummonerDTO.swift
@@ -0,0 +1,11 @@
+//
+//  SummonerDTO.swift
+//  RecommendLoLChams-Flab
+//
+//  Created by 김혜지 on 1/18/25.
+//
+
+// 소환사 DTO 작성중...
+struct SummonerDTO: Decodable {
+    let puuid: String
+}
--- RecommendLoLChams-Flab/Sources/UI/SearchViewModel.swift
+++ RecommendLoLChams-Flab/Sources/UI/SearchViewModel.swift
@@ -8,7 +8,28 @@
 import Foundation
 
 final class SearchViewModel: ObservableObject {
+    @Published var keyword: String = ""
+    @Published private(set) var summoners: [Summoner] = []
+
+    private let userSearchAPI: SummonerSearchAPI
+
+    init(userSearchAPI: SummonerSearchAPI) {
+        self.userSearchAPI = userSearchAPI
+    }
+
+    @MainActor
+    func clearKeyword() {
+        keyword = ""
+    }
+
     func search() async {
-        
+        do {
+            let summoners = try await userSearchAPI.searchUser(id: keyword)
+            await MainActor.run {
+                self.summoners = summoners
+            }
+        } catch {
+            print("error: \(error.localizedDescription)")
+        }
     }
 }
